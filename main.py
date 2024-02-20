a18384470988d2404ac4c380301ed65971f1c60d
dlskd255 <einstein1915@naver.com>
2024-02-20 18:14:00 +0900

    ‘cicd’

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "from the outside!!!"}
+    return {"Hello": "from the outside!!!!!!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
